b47285f220fb7137e473d807bddfd06d163ecef2
SHIV5000 <SHIVSURI1@GMAIL.COM>
2026-05-07 12:11:22 +0530

    Add Vite configuration with PWA support

---
+++ vite.config.js
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
+
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Talk & Task Enterprise',
+        short_name: 'Talk&Task',
+        description: 'Corporate Coordination Portal',
+        theme_color: '#008069',
+        icons: [
+          { src: 'icon-192.png', sizes: '192x192', type: 'image/png' },
+          { src: 'icon-512.png', sizes: '512x512', type: 'image/png' }
+        ]
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+      }
+    })
+  ]
+});
