7b6d85579b21a47e5751674c1097a3f9c66c01fd
SHIV5000 <SHIVSURI1@GMAIL.COM>
2026-05-09 15:42:58 +0530

    Update LeftSidebar.jsx

--- src/components/Sidebar/LeftSidebar.jsx
+++ src/components/Sidebar/LeftSidebar.jsx
@@ -106,10 +106,13 @@ export default function LeftSidebar({
         </div>
 
         {/* ★ The scrollable container now has an ID */}
-        <div
-  id="leftSidebarScroll"
-  className="flex-1 min-h-0 flex flex-col"
-  style={{ overflowY: "auto", scrollbarWidth: "thin", scrollbarColor: "rgba(255,255,255,0.4) rgba(255,255,255,0.05)" }}
+       id="leftSidebarScroll"
+  className="flex-1 flex flex-col"
+  style={{
+    overflowY: "scroll",
+    scrollbarWidth: "thin",
+    scrollbarColor: "rgba(255,255,255,0.4) rgba(255,255,255,0.08)"
+  }}
 >
           {myGroups.map((g) => {
             const hasUnread = messages.some(
