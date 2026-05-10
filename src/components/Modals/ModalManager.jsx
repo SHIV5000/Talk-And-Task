53c65f594c146d05a80cda8e6099ae3cd260b95c
SHIV5000 <SHIVSURI1@GMAIL.COM>
2026-05-10 12:25:44 +0530

    Implement ModalManager component to manage modals

---
+++ src/components/Modals/ModalManager.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import ContextMenuModal from './ContextMenuModal.jsx';
+import ProfileSettingsModal from './ProfileSettingsModal.jsx';
+import GroupFormModal from './GroupFormModal.jsx';
+import GroupSettingsModal from './GroupSettingsModal.jsx';
+import TaskTrailModal from './TaskTrailModal.jsx';
+import TaskConvertModal from './TaskConvertModal.jsx';
+import ReminderModal from './ReminderModal.jsx';
+import ScheduleSendModal from './ScheduleSendModal.jsx';
+import AdminEditUserModal from './AdminEditUserModal.jsx';
+import TaskAnalyticsModal from './TaskAnalyticsModal.jsx';
+import UploadOverlay from '../Common/UploadOverlay.jsx';
+
+export default function ModalManager(props) {
+    const { activeModal, isUploading, uploadProgress } = props;
+
+    // If no modal is active and nothing is uploading, render absolutely nothing (saves memory!)
+    if (!activeModal && !isUploading) return null;
+
+    return (
+        <>
+            {activeModal === 'context' && <ContextMenuModal {...props} />}
+            {activeModal === 'edit_profile' && <ProfileSettingsModal {...props} />}
+            {activeModal === 'group_form_modal' && <GroupFormModal {...props} />}
+            {activeModal === 'group_settings' && <GroupSettingsModal {...props} />}
+            {activeModal === 'task_trail' && props.selectedMessage?.taskData && <TaskTrailModal {...props} />}
+            {activeModal === 'task_convert' && <TaskConvertModal {...props} />}
+            {activeModal === 'reminder' && <ReminderModal {...props} />}
+            {activeModal === 'schedule_send' && <ScheduleSendModal {...props} />}
+            {activeModal === 'admin_edit_user' && <AdminEditUserModal {...props} />}
+            {activeModal === 'task_analytics' && <TaskAnalyticsModal {...props} />}
+            
+            {/* We also handle the Upload Progress overlay here since it acts like a modal */}
+            {isUploading && <UploadOverlay uploadProgress={uploadProgress} fileName="" />}
+        </>
+    );
+}
