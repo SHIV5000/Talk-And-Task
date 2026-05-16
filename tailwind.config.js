87f2d34e11321a27191e93343885ade01f15e400
SHIV5000 <SHIVSURI1@GMAIL.COM>
2026-05-16 22:40:55 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,8 @@
-
 // tailwind.config.js
-export default {
-  content:["./src/**/*.{js,jsx,ts,tsx}"],
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {
-      colors: {
-        ink: '#1e293b',       // For buttons/active states
-        offwhite: '#f8fafc',  // For main background
-        surface: '#ffffff',   // For cards/modals
-        text: {
-          main: '#0f172a',
-          muted: '#64748b',
-        },
-        border: '#e2e8f0',
-      },
-    },
+    extend: {},
   },
-  plugins:
+  plugins: [],
+};
